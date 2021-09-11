d30e0d2af4ca8405cadb65cf67d8052ca6491610
pahntanapat <pahntanapat@gmail.com>
2021-09-11 16:09:58 +0700

    add path

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 * @license Tanapat-Kahabodeekanokkul
 */
 
+const path = require('path');
 const colours = { reset: "[0m", bright: "[1m", dim: "[2m", underscore: "[4m", blink: "[5m", reverse: "[7m", hidden: "[8m", fg: { black: "[30m", red: "[31m", green: "[32m", yellow: "[33m", blue: "[34m", magenta: "[35m", cyan: "[36m", white: "[37m", crimson: "[38m" }, bg: { black: "[40m", red: "[41m", green: "[42m", yellow: "[43m", blue: "[44m", magenta: "[45m", cyan: "[46m", white: "[47m", crimson: "[48m" } };
 const
     EVENT_CLIENT_INIT = 'press-record',
@@ -161,13 +162,14 @@ function BindSIO(optionBindSIO, optionSIO) {
     return io;
 }
 
+
 function staticJsDir() {
     return path.resolve(__dirname, 'public/js')
 }
 
 function staticJsFiles() {
-    return require('fs').readdirSync(staticJsDir()).forEach((val, id) => {
-        this[id] = path.resolve(__dirname, 'public/js', val);
+    return require('fs').readdirSync(staticJsDir()).map((val) => {
+        return path.resolve(__dirname, 'public/js', val);
     });
 
 }
