62217bec9bc3a928bac592eca4840bafc85dc191
pahntanapat <pahntanapat@gmail.com>
2021-09-29 19:31:50 +0700

    remove unused

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const path = require('path');
 const pino = require('pino');
 
 
-const colours = { reset: "[0m", bright: "[1m", dim: "[2m", underscore: "[4m", blink: "[5m", reverse: "[7m", hidden: "[8m", fg: { black: "[30m", red: "[31m", green: "[32m", yellow: "[33m", blue: "[34m", magenta: "[35m", cyan: "[36m", white: "[37m", crimson: "[38m" }, bg: { black: "[40m", red: "[41m", green: "[42m", yellow: "[43m", blue: "[44m", magenta: "[45m", cyan: "[46m", white: "[47m", crimson: "[48m" } };
 const
     EVENT_CLIENT_INIT = 'press-record',
     EVENT_SERVER_SESSION_ID = 'sess-id',
